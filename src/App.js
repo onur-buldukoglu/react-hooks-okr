ad776cebb39d819543d16799eff7802e2bf6a191
Onur Buldukoglu <onur.buldukoglu@adesso.com.tr>
2022-12-27 22:50:37 +0300

    minor changes

--- src/App.js
+++ src/App.js
@@ -3,11 +3,7 @@ import { RouterProvider } from 'react-router-dom';
 import { router } from './libs/router';
 
 function App() {
-  return (
-    <RouterProvider router={router}>
-      <div />
-    </RouterProvider>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
