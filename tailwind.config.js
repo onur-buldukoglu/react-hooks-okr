239c0be69bdd2a94ab0b28cdf162366c1b38fe92
Onur Buldukoglu <onur.buldukoglu@adesso.com.tr>
2022-12-27 22:47:50 +0300

    edit tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx}"],
+  content: ['./src/**/*.{js,jsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: 'Helvetica, Arial, sans-serif',
+      },
+    },
   },
   plugins: [],
 };
